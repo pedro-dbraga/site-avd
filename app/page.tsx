8c531c4519d1eed5b314d64226dbcb13abdc23f1
Pedro Daniel Braga <pedrodanielbraga@outlook.com>
2025-10-20 17:25:35 -0300

    Botao direito Wapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 import { Header } from "./components/header";
 import { Content } from "./components/content";
@@ -12,6 +14,9 @@ export default function Home() {
       <Content/>
       <About/>
       <Footer/>
+      <a href="https://wa.me/5511921091441?text=Olá%2C+gostaria+de+desbloquear+minha+conta+bancária" className={styles.floatingwapp} target="_blank" rel="noopener noreferrer">
+        <Image src="/logowtapp.png" alt="WhatsApp" width={60} height={60}/>
+      </a>
     </main>
   );
 }
